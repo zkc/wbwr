d1f6c63550b6d663a94fd9c558ee9561a46991cb
Kyle Zucker <zkyle12@gmail.com>
2017-05-25 11:15:07 -0600

    fix play/pause button, lset always on top, position reader on open, style title bar

--- app/main.js
+++ app/main.js
@@ -1,15 +1,34 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray } = require('electron');
+const electron = require('electron')
 const Menubar = require('menubar');
 const windows = new Set()
 const menubar = Menubar({ width: 400, height: 500, icon: './images/white-owl.png' })
 
+
+let displaySize
+
+menubar.on('ready', () => {
+  console.log('wbwr ready');
+  displaySize = electron.screen.getAllDisplays()
+});
+
 const createWindow = exports.createWindow = (text) => {
-  let newWindow = new BrowserWindow({ width: 450, height: 220 });
+  let newWindow = new BrowserWindow({
+    x:((displaySize[0].bounds.width/2) - 225),
+    y:0,
+    minWidth: 450,
+    minHeight: 200,
+    width: 450,
+    height: 200,
+    titleBarStyle: 'hidden',
+    icon: './images/black-owl.png'
+  });
 
   windows.add(newWindow);
 
   newWindow.loadURL(`file://${__dirname}/reader.html`);
 
+  newWindow.setAlwaysOnTop(true, 'normal', 1);
   newWindow.readerText = text;
 
   newWindow.on('closed', () => {
@@ -17,10 +36,6 @@ const createWindow = exports.createWindow = (text) => {
     newWindow = null;
   });
 };
-
-menubar.on('ready', () => {
-  console.log('wbwr ready');
-});
 
 menubar.on('after-create-window', () => {
   menubar.window.loadURL(`file://${__dirname}/index.html`);
